d194c7eabd93474d581bd3c54ab09367eda993f3
hasansohailnaqi <138737153+hasansohailnaqi@users.noreply.github.com>
2024-02-08 20:50:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image'
+
+//import nav from './component/nav'
+//import Hero from './component/Hero'
+import Navbar from '../components/layout/Navbar'
+import Contact from '../components/Contact'
+//import Project from './component/Project'
+//import About from '../components/ui/AboutSection'
+import HeroSection from '@/components/Hero'
+import AboutSection from '../components/ui/AboutSection'
+import Project from '@/components/Project'
+//import Services from './component/services'
+export const dynamic = "force-dynamic";
+
+
+export default function Home() {
+  return (
+    <div>
+     
+     {/* <Navbar/>
+       <Hero />
+         <About />
+         <Services />
+         <Project />
+         <Contact />       
+  */}
+                <HeroSection/>
+                <AboutSection/>
+                <Project/>
+               {/* <Contact /> */}
+               
+    </div>
+
+    )
+    }
+    
+    
+    
+
+
+
+
+  
+
